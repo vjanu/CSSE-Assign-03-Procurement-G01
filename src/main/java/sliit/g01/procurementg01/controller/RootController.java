7cf163c23e1f57b6235fc3c17d79572eeab4e745
Anushka Yohan <aliyanage44@gmail.com>
2018-09-09 21:04:34 +0530

    Add error.html to static pages; Add all the required packages to Java section; Implement root controller that redirects to the index.html when the base url of the server is entered(i.e: localhost:8080).

---
+++ src/main/java/sliit/g01/procurementg01/controller/RootController.java
@@ -0,0 +1,20 @@
+package sliit.g01.procurementg01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String index() {
+        // index.html is at src/main/resources/static.
+        // we can reference anything at static directory as if we are in that directory.
+        return "<script type='text/javascript'> window.location.href = 'index.html'; </script>";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "<script type='text/javascript'> window.location.href = 'error.html'; </script>";
+    }
+}
