34f109af80b3f3b94fa0fa54aa5fb2fd83c2e3a0
Tharushi De Silva <desilva.watp@gmail.com>
2018-09-09 20:28:16 +0530

    Initial commit; SpringBoot application w/ MongoDB and Bootstraped static index.html page served from src/resources/static

---
+++ src/main/java/sliit/g01/procurementg01/ProcurementG01Application.java
@@ -0,0 +1,12 @@
+package sliit.g01.procurementg01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementG01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementG01Application.class, args);
+    }
+}
