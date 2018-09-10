13883d2cfc7a51115e5b3b75f31d9a818f0ebe8d
vjanu <vjanuradhawick@gmail.com>
2018-09-10 23:24:31 +0530

    AccountingStaff added

---
+++ src/main/java/sliit/g01/procurementg01/api/repository/AccountingStaffRepository.java
@@ -0,0 +1,17 @@
+package sliit.g01.procurementg01.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import sliit.g01.procurementg01.api.model.AccountingStaff;
+/***
+ * created by viraj
+ ***/
+public interface AccountingStaffRepository extends MongoRepository<AccountingStaff, String> {
+
+	AccountingStaff findByEmployeeId(String employeeId);
+    List<AccountingStaff> findByDeleted(boolean deleted); 
+    List<AccountingStaff> findByCategoryAndDeleted(String status, boolean deleted);
+
+}
