359f6cb74325d5f7cfe00f7688eb434d811f414d
tharindutcj <tharindutcj@gmail.com>
2018-09-13 23:05:16 +0530

    Category methods implemented using service

--- src/main/java/sliit/g01/procurementg01/api/model/Category.java
+++ src/main/java/sliit/g01/procurementg01/api/model/Category.java
@@ -1,19 +1,37 @@
 package sliit.g01.procurementg01.api.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "Category")
 public class Category {
-	
-	@Id
-    private String categoryId;
-    private String categoryName;
-    
-    public void selectCategory(String categoryId) {
-        this.categoryId = categoryId;
-    }
 
-    public boolean addCategory(String categoryId, String categoryName) {
-        return false;
-    }
+	@Id
+	private String categoryId;
+	private String categoryName;
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public void selectCategory(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public boolean addCategory(String categoryId, String categoryName) {
+		return false;
+	}
 
 }
