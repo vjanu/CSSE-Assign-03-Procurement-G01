8b26e635236ea9584b79a3f1a2d632a29cacbe5b
athnath <32517652+athnath@users.noreply.github.com>
2018-09-16 11:25:12 +0530

    add status types

--- src/main/java/sliit/g01/procurementg01/api/controller/RequestMaterialController.java
+++ src/main/java/sliit/g01/procurementg01/api/controller/RequestMaterialController.java
@@ -9,19 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import sliit.g01.procurementg01.api.model.RequestMaterial;
 
 import sliit.g01.procurementg01.api.service.RequestMaterialService;
 
+
+@RestController
+@RequestMapping("/requestmaterial")
 public class RequestMaterialController {
 	@Autowired
-	private RequestMaterialService request_material;
+	private RequestMaterialService requestmaterial;
 
 	@PostMapping("/add-new-order")
 	public ResponseEntity<String> addOrder(@RequestBody RequestMaterial requestMaterial) {
-		if (request_material.addOrder(requestMaterial)) {
+		if (requestmaterial.addOrder(requestMaterial)) {
 			return new ResponseEntity<>("New order Added", HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>("Not Created", HttpStatus.NOT_IMPLEMENTED);
@@ -31,11 +35,11 @@ public class RequestMaterialController {
 
 	@GetMapping("/")
 	public List<RequestMaterial> getAllOrders() {
-		return request_material.getAllOrders();
+		return requestmaterial.getAllOrders();
 	}
 
 	@GetMapping("/{orderId}")
 	public RequestMaterial getOrder(@PathVariable String orderId) {
-		return request_material.getOrder(orderId);
+		return requestmaterial.getOrder(orderId);
 	}
 }
