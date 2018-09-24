0b2949946a597b24131ce67d2b6ca3cc93e2c965
littlemzsplendid <desilva.watp@gmail.com>
2018-09-24 22:53:35 +0530

    Modified supplier.java and added supplier service

--- src/main/java/sliit/g01/procurementg01/api/model/Supplier.java
+++ src/main/java/sliit/g01/procurementg01/api/model/Supplier.java
@@ -13,7 +13,54 @@ public class Supplier {
     private String email;
     private String phone;
     private boolean availability;
+	
+    // setters
+	public void setSupplierId(String supplierId) {
+		this.supplierId = supplierId;
+	}
+	
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	
+	public void setBankAccountNo(int bankAccountNo) {
+		this.bankAccountNo = bankAccountNo;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setAvailability(boolean availability) {
+		this.availability = availability;
+	}
 
-    
-
+	// Getters
+	public String getSupplierId() {
+		return supplierId;
+	}
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public int getBankAccountNo() {
+		return bankAccountNo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public boolean isAvailability() {
+		return availability;
+	}
 }
---
+++ src/main/java/sliit/g01/procurementg01/api/service/SupplierService.java
@@ -0,0 +1,12 @@
+package sliit.g01.procurementg01.api.service;
+
+import java.util.List;
+import sliit.g01.procurementg01.api.model.Supplier;
+
+public interface SupplierService {
+	boolean addSupplier( Supplier supplier);
+	List<Supplier> getAllSuppliers();
+	Supplier getSupplier(String supplierId);
+	Boolean deleteSupplier(String supplierId);
+	
+}
