11ba64fcf3b3c7e2e14153271fdf1bbcf239ff39
littlemzsplendid <desilva.watp@gmail.com>
2018-09-25 00:07:45 +0530

    Added supplier repository and service impl

---
+++ src/main/java/sliit/g01/procurementg01/api/repository/SupplierRepository.java
@@ -0,0 +1,10 @@
+package sliit.g01.procurementg01.api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import sliit.g01.procurementg01.api.model.Supplier;
+
+public interface SupplierRepository extends MongoRepository <Supplier, String> {
+	Supplier findBySupplierId (String supplierId);
+	
+}
--- src/main/java/sliit/g01/procurementg01/api/service/SupplierService.java
+++ src/main/java/sliit/g01/procurementg01/api/service/SupplierService.java
@@ -5,8 +5,11 @@ import sliit.g01.procurementg01.api.model.Supplier;
 
 public interface SupplierService {
 	boolean addSupplier( Supplier supplier);
+	
 	List<Supplier> getAllSuppliers();
+	
 	Supplier getSupplier(String supplierId);
+	
 	Boolean deleteSupplier(String supplierId);
 	
 }
