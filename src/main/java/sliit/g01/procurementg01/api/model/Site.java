27f33f8797d5e618944ebb96314bc6a34631d7db
Tharindu <tharinduewings@gmail.com>
2018-10-04 17:05:03 +0530

    Edit site implemented

--- src/main/java/sliit/g01/procurementg01/api/model/Site.java
+++ src/main/java/sliit/g01/procurementg01/api/model/Site.java
@@ -1,7 +1,6 @@
 package sliit.g01.procurementg01.api.model;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "Site")
 public class Site {
 
-    @Id
+	@Id
 	private String siteId;
 	private String siteName;
 	private String address;
--- src/main/java/sliit/g01/procurementg01/api/service/SiteService.java
+++ src/main/java/sliit/g01/procurementg01/api/service/SiteService.java
@@ -1,7 +1,6 @@
 package sliit.g01.procurementg01.api.service;
 
 import java.util.List;
-import java.util.Map;
 
 import sliit.g01.procurementg01.api.model.Item;
 import sliit.g01.procurementg01.api.model.Site;
@@ -21,7 +20,7 @@ public interface SiteService {
 
 	boolean deleteSite(String siteId);
 
-	Site updateSite(String siteId, Site site);
+	boolean updateSite(String siteId, Site site);
 
 	Site getSiteUnderManager(String siteManagerId);
 
